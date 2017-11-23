c9ca404a34d0e204f831af1969e9cc5bfd6c2ae0
Kairi Kozuma <kairikozuma@gmail.com>
2017-11-22 23:54:15 -0500

    Create makefile and use local config for spouts/bolts

--- storm-server/storm-project/multilang/resources/twitter_spout.py
+++ storm-server/storm-project/multilang/resources/twitter_spout.py
@@ -4,8 +4,7 @@ import os
 import json
 
 # Load config file
-abs_path = os.path.dirname(os.path.abspath(__file__))
-with open('{}/../../../config.json'.format(abs_path)) as json_data_file:
+with open('config.json') as json_data_file:
     config = json.load(json_data_file)
 
 # Create Kafka consumer
@@ -17,7 +16,7 @@ class TweetSpout(Spout):
 
     def nextTuple(self):
         # Consume next data from Kafka queue
-    	data = consumer.next().value
+    	data = kafka_consumer.next().value
 
         # Emit value so bolts down stream can process it
         emit([data])

073791d7394ad58537c53145e9feaea3ba98fde1
Ruo Zhang <zhangruo1117@gatech.edu>
2017-11-29 17:06:17 -0500

    Spouts
    Needs to be renamed

---
+++ storm-server/storm-project/multilang/resources/news_sprout.py
@@ -0,0 +1,24 @@
+from storm import Spout, emit, log
+from kafka import KafkaConsumer
+import os
+import json
+
+# Load config file
+with open('config.json') as json_data_file:
+    config = json.load(json_data_file)
+
+# Create Kafka consumer
+servers = config['kafka']['servers']
+topic = config['kafka']['topic']['news']
+kafka_consumer = KafkaConsumer(topic, bootstrap_servers=servers)
+
+class NewSpout(Spout):
+
+    def nextTuple(self):
+        # Consume next data from Kafka queue
+    	data = kafka_consumer.next().value
+
+        # Emit value so bolts down stream can process it
+        emit([data])
+		
+NewsSpout().run()
